381b3d92e92979662bf7f454af1ff757480c48cb
educaysoft <educaysoft@gmail.com>
2021-07-15 11:45:03 -0500

    Pagina principal para la facultad de facae
    Esta es la pagina principal

---
+++ index.php
@@ -0,0 +1,84 @@
+<html>
+<head>
+
+<title> Documentos FACAE</title>
+
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<style>
+body{
+display: flex;
+min-height: 100vh;
+min-width: 100vh;
+flex-direction: column;
+margin: 0;
+}
+
+#cabeza{
+background-color: red;
+flex: 0.2;
+}
+#cuerpo{
+background-color: red;
+flex: 0.6;
+}
+#pie{
+background-color: red;
+flex: 0.2;
+}
+
+
+</style>
+
+
+
+
+</head>
+
+<body>
+
+<div id="cabeza" >
+
+
+</div>
+
+<div id="cuerpo">
+
+
+<div class="w3-card-4" style=" position: relative; padding: 10px; width: 80%; height: 80%;">
+<div></div>
+<div></div>
+<div></div>
+
+
+</div>
+
+
+</div>
+
+<div id="pie">
+
+
+</div>
+
+
+
+
+
+
+
+
+</body>
+
+
+
+
+
+
+
+
+
+
+
+</html>
+
+
