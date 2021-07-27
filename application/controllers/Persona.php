1a24a2232ce2fc2bb279634fd744f29ef12f4f37
educaysoft <educaysoft@gmail.com>
2021-07-27 12:28:17 -0500

    Add files via upload

---
+++ application/controllers/Persona.php
@@ -0,0 +1,18 @@
+<?php
+
+class Persona extends CI_Controller{
+
+  public function __construct(){
+      parent::__construct();
+      $this->load->model('persona_model');
+}
+
+public function index(){
+  $data['persona_list']=$this->persona_model->listar_persona()->result();
+ // print_r($data['persona_list']);
+  $data['title']="LIsta de Persona";
+  $this->load->view('persona_list',$data);
+}
+
+
+}
