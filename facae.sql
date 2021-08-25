282b433e8fca8e25a58bc6118c32e3e47001c8f8
Stalin Francis <educaysoft@gmail.com>
2021-08-25 11:28:34 -0500

    Nueva Actualizando el modelo de base de datos

--- facae.sql
+++ facae.sql
@@ -1,4 +1,209 @@
-Usage: mysqldump [OPTIONS] database [tables]
-OR     mysqldump [OPTIONS] --databases [OPTIONS] DB1 [DB2 DB3...]
-OR     mysqldump [OPTIONS] --all-databases [OPTIONS]
-For more options, use mysqldump --help
+-- MySQL dump 10.13  Distrib 8.0.26, for Linux (x86_64)
+--
+-- Host: localhost    Database: facae
+-- ------------------------------------------------------
+-- Server version	8.0.26
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `asignatura`
+--
+
+DROP TABLE IF EXISTS `asignatura`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `asignatura` (
+  `idasignatura` int NOT NULL,
+  `nombre` varchar(45) DEFAULT NULL,
+  `creditos` int DEFAULT NULL,
+  PRIMARY KEY (`idasignatura`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `asignatura`
+--
+
+LOCK TABLES `asignatura` WRITE;
+/*!40000 ALTER TABLE `asignatura` DISABLE KEYS */;
+/*!40000 ALTER TABLE `asignatura` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `docente`
+--
+
+DROP TABLE IF EXISTS `docente`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `docente` (
+  `iddocente` int NOT NULL AUTO_INCREMENT,
+  `idpersona` int NOT NULL,
+  PRIMARY KEY (`iddocente`,`idpersona`),
+  KEY `fk_docente_persona1_idx` (`idpersona`),
+  CONSTRAINT `fk_docente_persona1` FOREIGN KEY (`idpersona`) REFERENCES `persona` (`idpersona`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `docente`
+--
+
+LOCK TABLES `docente` WRITE;
+/*!40000 ALTER TABLE `docente` DISABLE KEYS */;
+/*!40000 ALTER TABLE `docente` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `estudiane`
+--
+
+DROP TABLE IF EXISTS `estudiane`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `estudiane` (
+  `idestudiane` int NOT NULL AUTO_INCREMENT,
+  `idpersona` int NOT NULL,
+  PRIMARY KEY (`idestudiane`,`idpersona`),
+  KEY `fk_estudiane_persona1_idx` (`idpersona`),
+  CONSTRAINT `fk_estudiane_persona1` FOREIGN KEY (`idpersona`) REFERENCES `persona` (`idpersona`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `estudiane`
+--
+
+LOCK TABLES `estudiane` WRITE;
+/*!40000 ALTER TABLE `estudiane` DISABLE KEYS */;
+/*!40000 ALTER TABLE `estudiane` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `persona`
+--
+
+DROP TABLE IF EXISTS `persona`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `persona` (
+  `idpersona` int NOT NULL AUTO_INCREMENT,
+  `nombres` varchar(50) DEFAULT NULL,
+  `apellidos` varchar(50) DEFAULT NULL,
+  `cedula` varchar(10) DEFAULT NULL,
+  `fechanaci` date DEFAULT NULL,
+  `foto` char(50) DEFAULT NULL,
+  `pdf` char(50) DEFAULT NULL,
+  `idgenero` int DEFAULT NULL,
+  `idtiposangre` int DEFAULT NULL,
+  `idestadocivil` int DEFAULT NULL,
+  `idnacionalidad` int DEFAULT NULL,
+  PRIMARY KEY (`idpersona`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `persona`
+--
+
+LOCK TABLES `persona` WRITE;
+/*!40000 ALTER TABLE `persona` DISABLE KEYS */;
+INSERT INTO `persona` VALUES (5,'FRANCIS QUINDE STALIN ADALBERTO',NULL,'0801560517',NULL,'fotos/0801560517.jpg','pdfs/0801560517.pdf',1,1,1,1);
+/*!40000 ALTER TABLE `persona` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `portafoliod`
+--
+
+DROP TABLE IF EXISTS `portafoliod`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `portafoliod` (
+  `idportafoliod` int NOT NULL AUTO_INCREMENT,
+  `color` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `hojadevida` tinyint DEFAULT NULL,
+  `hojadevidapdf` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `silabo` tinyint DEFAULT NULL,
+  `silabopdf` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `cuadernillo` tinyint DEFAULT NULL,
+  `cuadernillopdf` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `horario` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `horariopdf` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `plancatedra` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `plancatedrapdf` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `reportenotas` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `reportenotaspdf` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `informelabores` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `informelaborespdf` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `horarioexamen` varchar(45) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `horarioexamenpdf` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `observaciones` text COLLATE utf8mb4_general_ci,
+  `iddocente` int NOT NULL,
+  `idasignatura` int NOT NULL,
+  PRIMARY KEY (`idportafoliod`,`iddocente`,`idasignatura`),
+  KEY `fk_portafoliod_docente1_idx` (`iddocente`),
+  KEY `fk_portafoliod_asignatura1_idx` (`idasignatura`),
+  CONSTRAINT `fk_portafoliod_asignatura1` FOREIGN KEY (`idasignatura`) REFERENCES `asignatura` (`idasignatura`),
+  CONSTRAINT `fk_portafoliod_docente1` FOREIGN KEY (`iddocente`) REFERENCES `docente` (`iddocente`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `portafoliod`
+--
+
+LOCK TABLES `portafoliod` WRITE;
+/*!40000 ALTER TABLE `portafoliod` DISABLE KEYS */;
+/*!40000 ALTER TABLE `portafoliod` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuario` (
+  `idusuario` int NOT NULL,
+  `password` varchar(45) DEFAULT NULL,
+  `idpersona` int DEFAULT NULL,
+  `idperfil` int DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`idusuario`),
+  KEY `idpersona` (`idpersona`),
+  CONSTRAINT `usuario_ibfk_1` FOREIGN KEY (`idpersona`) REFERENCES `persona` (`idpersona`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuario`
+--
+
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+INSERT INTO `usuario` VALUES (0,'PIWIIB1234',5,NULL,'educaysoft@gmail.com');
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-08-25 11:27:58
