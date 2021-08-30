26f17c2cb75d1d42f926a40ff2ec7d3d7932bd63
KevinAguilar16 <87490938+KevinAguilar16@users.noreply.github.com>
2021-08-30 11:15:45 -0500

    ADD PERFIL
    CONTROLADOR DE PERFIL

---
+++ application/controllers/Perfil.php
@@ -0,0 +1,71 @@
+<?php
+
+class Perfil extends CI_Controller{
+
+  public function __construct(){
+      parent::__construct();
+      $this->load->model('perfil_model');
+}
+
+public function index(){
+  $data['perfil_list']=$this->perfil_model->listar_perfiles()->result();
+ // print_r($data['usuario_list']);
+  $data['title']="LIsta de Perfiles";
+	$this->load->view('template/page_header');		
+  $this->load->view('perfil_list',$data);
+	$this->load->view('template/page_footer');
+}
+
+
+public function add()
+{
+		$data['title']="Nuevo Perfil";
+	 	$this->load->view('template/page_header');		
+	 	$this->load->view('perfil_form',$data);
+	 	$this->load->view('template/page_footer');
+
+
+}
+
+
+	public function  save()
+	{
+	 	$array_item=array(
+		 	
+		 	'idperfil' => $this->input->post('idpefil'),
+		 	'descripcion' => $this->input->post('descripcion'),
+	 	);
+	 	$this->perfil_model->save($array_item);
+	 	redirect('perfil');
+ 	}
+
+
+
+public function edit()
+{
+	 	$data['perfil'] = $this->perfil_model->perfil($this->uri->segment(3))->row_array();
+ 	 	$data['title'] = "Actualizar Persona";
+ 	 	$this->load->view('template/page_header');		
+ 	 	$this->load->view('perfil_edit',$data);
+	 	$this->load->view('template/page_footer');
+ 
+}
+
+
+	public function  save_edit()
+	{
+		$id=$this->input->post('idperfil');
+	 	$array_item=array(
+		 	
+		 	'idperfil' => $this->input->post('idpefil'),
+		 	'descripcion' => $this->input->post('descripcion'),
+	 	);
+	 	$this->perfil_model->update($id,$array_item);
+	 	redirect('perfil');
+ 	}
+
+
+
+
+
+}
