9bdb19a0514e703bc69782e46aeb49a28db0a05a
KevinAguilar16 <87490938+KevinAguilar16@users.noreply.github.com>
2021-09-10 00:30:31 -0500

    CONTROLADOR DE INSTITUCION

---
+++ application/controllers/Institucion.php
@@ -0,0 +1,71 @@
+<?php
+
+class Institucion extends CI_Controller{
+
+  public function __construct(){
+      parent::__construct();
+      $this->load->model('institucion_model');
+}
+
+public function index(){
+  $data['institucion_list']=$this->institucion_model->lista_institucion()->result();
+ // print_r($data['usuario_list']);
+  $data['title']="LIsta de Instituciones";
+	$this->load->view('template/page_header');		
+  $this->load->view('institucion_list',$data);
+	$this->load->view('template/page_footer');
+}
+
+
+public function add()
+{
+		$data['title']="Nueva Institucion";
+	 	$this->load->view('template/page_header');		
+	 	$this->load->view('institucion_form',$data);
+	 	$this->load->view('template/page_footer');
+
+
+}
+
+
+	public function  save()
+	{
+	 	$array_item=array(
+		 	
+		 	'idinstitucion' => $this->input->post('idinstitucion'),
+		 	'nombre' => $this->input->post('nombre'),
+	 	);
+	 	$this->institucion_model->save($array_item);
+	 	redirect('institucion');
+ 	}
+
+
+
+public function edit()
+{
+	 	$data['institucion'] = $this->institucion_model->institucion($this->uri->segment(3))->row_array();
+ 	 	$data['title'] = "Actualizar Persona";
+ 	 	$this->load->view('template/page_header');		
+ 	 	$this->load->view('institucion_edit',$data);
+	 	$this->load->view('template/page_footer');
+ 
+}
+
+
+	public function  save_edit()
+	{
+		$id=$this->input->post('idinstitucion');
+	 	$array_item=array(
+		 	
+		 	'idinstitucion' => $this->input->post('idinstitucion'),
+		 	'nombre' => $this->input->post('nombre'),
+	 	);
+	 	$this->institucion_model->update($id,$array_item);
+	 	redirect('institucion');
+ 	}
+
+
+
+
+
+}
