dd869d0500d8019c016e4ae6914b5fa9041c8d5a
KevinAguilar16 <87490938+KevinAguilar16@users.noreply.github.com>
2021-09-10 00:58:51 -0500

    Controlador de Departamento

---
+++ application/controllers/Departamento.php
@@ -0,0 +1,71 @@
+<?php
+
+class Departamento extends CI_Controller{
+
+  public function __construct(){
+      parent::__construct();
+      $this->load->model('departamento_model');
+}
+
+public function index(){
+  $data['departamento_list']=$this->departamento_model->lista_departamento()->result();
+ // print_r($data['usuario_list']);
+  $data['title']="Lista de Departamentos";
+	$this->load->view('template/page_header');		
+  $this->load->view('departamento_list',$data);
+	$this->load->view('template/page_footer');
+}
+
+
+public function add()
+{
+		$data['title']="Nuevo Departamento";
+	 	$this->load->view('template/page_header');		
+	 	$this->load->view('departamento_form',$data);
+	 	$this->load->view('template/page_footer');
+
+
+}
+
+
+	public function  save()
+	{
+	 	$array_item=array(
+		 	
+		 	'iddepartamento' => $this->input->post('iddepartamento'),
+		 	'nombre' => $this->input->post('nombre'),
+	 	);
+	 	$this->departamento_model->save($array_item);
+	 	redirect('departamento');
+ 	}
+
+
+
+public function edit()
+{
+	 	$data['departamento'] = $this->departamento_model->departamento($this->uri->segment(3))->row_array();
+ 	 	$data['title'] = "Actualizar Persona";
+ 	 	$this->load->view('template/page_header');		
+ 	 	$this->load->view('departamento_edit',$data);
+	 	$this->load->view('template/page_footer');
+ 
+}
+
+
+	public function  save_edit()
+	{
+		$id=$this->input->post('iddepartamento');
+	 	$array_item=array(
+		 	
+		 	'iddepartamento' => $this->input->post('iddepartamento'),
+		 	'nombre' => $this->input->post('nombre'),
+	 	);
+	 	$this->departamento_model->update($id,$array_item);
+	 	redirect('departamento');
+ 	}
+
+
+
+
+
+}
